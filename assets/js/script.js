18e8d9649979b20ae74ce8b29a38a2d199aac1ad
C Bares <colinedwinbares@gmail.com>
2022-01-05 14:54:53 -0600

    set up a dummy api call in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
 var theMovieDbUrl = "https://api.themoviedb.org/3/movie/550?api_key="; 
 var theMovieDbApiKey = config.theMovieDbApiKey;
+
+var getMovieData = function() {
+    fetch(theMovieDbUrl+theMovieDbApiKey)
+    .then(function(response) {
+        response.json()
+        .then(function(data) {
+            console.log(data);
+        })
+    })
+}
