b4e7ad6cc416d4b3093e25a9492baa11945c6c64
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-01-06 20:47:58 -0600

    made some changes to two functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // base url for api calls
-var theMovieDbUrl = "https://api.themoviedb.org/3/"; 
+var theMovieDbUrl = "https://api.themoviedb.org/3/";
 // api key pulled from config file
 var theMovieDbApiKey = config.theMovieDbApiKey;
 
@@ -10,56 +10,60 @@ let configData = null;
 let posterSize = null;
 
 // get configuration data from TMDB api
-var getConfigInfo = function() {
-    fetch('https://api.themoviedb.org/3/configuration?api_key=' + theMovieDbApiKey)
-    .then(function(response) {
-        response.json()
-        .then(function(data) {
-            imgBaseUrl = data.images.secure_base_url;
-            configData = data.images;
-            posterSize = data.images.poster_sizes[1];
-        })
-    })
+var getConfigInfo = function () {
+  fetch(
+    "https://api.themoviedb.org/3/configuration?api_key=" + theMovieDbApiKey
+  ).then(function (response) {
+    response.json().then(function (data) {
+      imgBaseUrl = data.images.secure_base_url;
+      configData = data.images;
+      posterSize = data.images.poster_sizes[1];
+    });
+  });
 };
 
 // get the config data after document loads
-document.addEventListener('DOMContentLoaded', getConfigInfo);
+document.addEventListener("DOMContentLoaded", getConfigInfo);
 
 // grabbing buttons from html
-let top10Btn = document.querySelector('#top10Btn');
-let myFavBtn = document.querySelector('#myFav');
-let genreSelBtn = document.querySelector('#genreSel');
-let runTimeBtn = document.querySelector('#runTime');
+let top10Btn = document.querySelector("#top10Btn");
+let genreSelBtn = document.querySelector("#genreSel");
+let watchlistBtn = document.querySelector("#watchlist");
 // event listener to pull trending movies
-top10Btn.addEventListener('click', function() {
-    getTopTen();
+top10Btn.addEventListener("click", function () {
+  getTopTen();
+});
+
+// event listener to pull genre options for user to select
+genreSelBtn.addEventListener("click", function () {
+  loadGenres();
 });
 
 // https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>
 // fetch movie data from TMDB API
-var getTopTen = function() {
-    fetch(`${theMovieDbUrl}trending/all/day?api_key=${theMovieDbApiKey}`)
-    .then(function(response) {
-        response.json()
-        .then(function(data) {
-            let results = data.results;
-            console.log(results);
-            for (let i = 0; i < 9; i++) {
-                let id = results[i].id;
-                fetch(`${theMovieDbUrl}movie/${id}?api_key=${theMovieDbApiKey}`)
-                .then(function(response) {
-                    response.json()
-                    .then(function(data) {
-                        console.log(data);
-                        let posterPath = data.poster_path;
-                        let fullUrl = `${imgBaseUrl}/${posterSize}/${posterPath}`
-                        let imgBlock = document.querySelector(`#movie${i+1}`);
-                        imgBlock.setAttribute('src', fullUrl);
-                    })
-                })
+var getTopTen = function () {
+  fetch(`${theMovieDbUrl}trending/all/day?api_key=${theMovieDbApiKey}`).then(
+    function (response) {
+      response.json().then(function (data) {
+        let results = data.results;
+        console.log(results);
+        for (let i = 0; i < 9; i++) {
+          let id = results[i].id;
+          fetch(`${theMovieDbUrl}movie/${id}?api_key=${theMovieDbApiKey}`).then(
+            function (response) {
+              response.json().then(function (data) {
+                console.log(data);
+                let posterPath = data.poster_path;
+                let fullUrl = `${imgBaseUrl}/${posterSize}/${posterPath}`;
+                let imgBlock = document.querySelector(`#movie${i + 1}`);
+                imgBlock.setAttribute("src", fullUrl);
+              });
             }
-        })
-    })
+          );
+        }
+      });
+    }
+  );
 };
 
 // TODO: Update top ten for movie && | show -- Colin
@@ -69,6 +73,67 @@ var getTopTen = function() {
 
 // TODO: Gene selector -- using genre ids filter choices: let them choose from list using if() statement -- Claire
 
+// Fetch genre options from TMDB api, targeting genre ids
+var loadGenres = function () {
+  fetch(
+    theMovieDbUrl +
+      "genre/movie/list?api_key=" +
+      theMovieDbApiKey +
+      "&language=en-US"
+  ).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    
+      // viewing genre ids from array as dynamic buttons on html
+      var parentDivEl = document.querySelector("#genre-list");
+
+      for (var i = 0; i < data.genres.length; i++) {
+        
+        var genreDataId = data.genres[i].id;
+
+        var button = document.createElement("button");
+        console.log("button", button);
+
+        button.setAttribute("id", data.genres[i].id);
+        button.setAttribute("class", data.genres[i].name);
+        button.textContent = data.genres[i].name;
+        console.log(button);
+
+        button.addEventListener("click", function(){
+            searchByGenre(genreDataId);
+        });
+        parentDivEl.appendChild(button);
+      }
+    });
+  });
+};
+
+var searchByGenre = function (genreDataId) {
+    console.log(genreDataId);
+    event.target()
+    // fetch( theMovieDbUrl + "discover/movie?api_key=" + theMovieDbApiKey + "&with_genres=37&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
+    // .then(function (response){
+    // response.json().then(function (data) {
+    //   console.log(data);})})
+}
+
+// function searchByGenre(genres) {
+//     console.log("is this happening?");
+//   // Here the user will select ONE genre as a filter and
+// //   fetch(
+// //     theMovieDbUrl +
+// //       "genre/movie/list?api_key=" +
+// //       theMovieDbApiKey +
+// //       "&language=en-US"
+// //   ).then(function (response) {
+// //     response.json().then(
+// //       function (data) {
+// //         var results = data;
+// //       } // on click, results of 10 movies for that genre will return to display.
+// //     );
+// //   }); 
+// }
+
 // TODO: Keyword search can filter request using Search, Discover, or Keyword API; clear input after submit -- Omar
 
-// TODO: Jake and Omar on CSS :)
+// TODO: Jake and Omar on CSS
